f8c34f9f3143db4ff9c12531208cf98bd2f4e9e5
Michael Zingale <michael.zingale@stonybrook.edu>
2019-04-03 09:34:18 -0400

    for 2nd order SDC, use cur_time in the sources to ensure that the FillPatch in diffusion operates on the correct state

--- Source/driver/Castro_advance_sdc.cpp
+++ Source/driver/Castro_advance_sdc.cpp
@@ -115,7 +115,7 @@ Castro::do_advance_sdc (Real time,
         }
 
       } else {
-        do_old_sources(old_source, Sborder, node_time, dt, amr_iteration, amr_ncycle);
+        do_old_sources(old_source, Sborder, cur_time, dt, amr_iteration, amr_ncycle);
       }
 
       // note: we don't need a FillPatch on the sources, since they
